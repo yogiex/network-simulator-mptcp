b0338ac0c12361022ad396847ef7504c411f9ff6
yogiex <priyagung.yoggi@gmail.com>
2020-10-22 00:04:29 +0700

    progress malam

--- pembelajaran.tcl
+++ pembelajaran.tcl
@@ -1,6 +1,25 @@
 #Create a simulator object
 set ns [new Simulator]
 
+#membuat file trace
+set file [open fileTrace.tr w]
+$ns trace-all $file
+
+#init node
+set n0 [$ns node]
+set n1 [$ns node]
+
 #Define different colors for data flows (for NAM)
-$ns color 1 Blue
-$ns color 2 Red
+$n0 color 1 Blue
+$n1 color 2 Red
+
+$ns duplex-link $ns0 $n1 1Mb 10Ms DropTail
+$ns duplex-link $ns0 $n1 1Mb 5Ms DropTail
+$ns duplex-link $ns0 $n1 5Mb 10Ms DropTail
+
+#membuat traffic TCP biasa
+set tcp0 [new Agent/TCP]
+$ns attach-agent $n0 $tcp0
+
+#create CBR traffic 
+set cbr0 []
