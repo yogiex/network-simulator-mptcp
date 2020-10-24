8387af6c093db26a5d0390bb9a138f15616cc4aa
yogiex <priyagung.yoggi@gmail.com>
2020-10-24 21:11:43 +0700

    malam minggu

--- pembelajaran.tcl
+++ pembelajaran.tcl
@@ -5,9 +5,17 @@ set ns [new Simulator]
 set file [open fileTrace.tr w]
 $ns trace-all $file
 
-#init node
+#init node n0
 set n0 [$ns node]
+set n0_1 [$ns node]
+set n0_2 [$ns node]
+
+
+
+#init node n1
 set n1 [$ns node]
+set n1_1 [$ns node]
+set n1_2 [$ns node]
 
 #Define different colors for data flows (for NAM)
 $n0 color 1 Blue
@@ -15,7 +23,7 @@ $n1 color 2 Red
 
 $ns duplex-link $ns0 $n1 1Mb 10Ms DropTail
 $ns duplex-link $ns0 $n1 1Mb 5Ms DropTail
-$ns duplex-link $ns0 $n1 5Mb 10Ms DropTail
+
 
 #membuat traffic TCP biasa
 set tcp0 [new Agent/TCP]
